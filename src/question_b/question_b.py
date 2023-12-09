e1507d93e698b544133b01533fbb72cbdbe2dc64
CannonBrave <142853122+CannonBrave@users.noreply.github.com>
2023-12-09 19:04:04 +0000

    Multiplication Table (question_b)

--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,19 @@
-#write functions here, don't add input('') statements here!
+def create_multiplication_table():
+    table = []
+    for i in range(1, 11):
+        row = [i * j for j in range(1, 11)]
+        table.append(row)
+    return table
+
+def display_multiplication_table(table):
+    for row in table:
+        print(" ".join(f"{num:2}" for num in row))
+
+def main():
+    while True:
+        table = create_multiplication_table()
+        display_multiplication_table(table)
+
+        exit = input("Do you want to display the table again? (y/n): ")
+        if exit != 'y':
+            break
