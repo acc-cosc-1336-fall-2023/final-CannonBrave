b4c91d874aa2720e4b7dec5183393a526de838e1
CannonBrave <142853122+CannonBrave@users.noreply.github.com>
2023-12-09 19:05:43 +0000

    DNA strings comparison (question_c)

--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -1,1 +1,21 @@
-#write functions here, don't add input('') statements here!
+def get_most_likely_ancestor_consensus(dna_string1, dna_string2):
+    positions = []
+    for i in range(len(dna_string1) - len(dna_string2) + 1):
+        if dna_string1[i:i+len(dna_string2)] == dna_string2:
+            positions.append(i + 1)
+    return positions
+
+def main():
+    while True:
+        dna_string1 = input("Enter DNA string (8-16 characters): ")
+        dna_string2 = input("Enter a DNA substring (exactly 4 characters): ")
+
+        if not (8 <= len(dna_string1) <= 16) or len(dna_string2) != 4:
+            print("Invalid input. Please try again.")
+            continue
+
+        positions = get_most_likely_ancestor_consensus(dna_string1, dna_string2)
+        print("Positions:", " ".join(map(str, positions)))
+
+        if input("Try again? Enter yes or no: ") != "yes":
+            break
--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -2,11 +2,20 @@
 import unittest
 
 #follow this example to add questions b, c, and d for testing including their functions
-from src.question_a.question_a import test_config
+#from src.question_a.question_a import test_config
+from src.question_c.question_c import get_most_likely_ancestor_consensus
 
 class Test_Config(unittest.TestCase):
 
-    def test_question_a_config(self):
-        self.assertEqual(True, test_config())
+    #def test_question_a_config(self):
+    #    self.assertEqual(True, test_config())
 
+    def test_get_most_likely_ancestor_consensus(self):
+        dna_string1 = "GATATATGCATATACTT"
+        dna_string2 = "ATAT"
+        expected_result = [2, 4, 10]
+        
+        test_result = get_most_likely_ancestor_consensus(dna_string1, dna_string2)
+        
+        self.assertEqual(test_result, expected_result)
 
